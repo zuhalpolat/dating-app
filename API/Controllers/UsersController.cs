a8cd8b25cc66162a1d81bb6d19059d32326eb45d
Zuhal Polat <zuhalpolat55@gmail.com>
2021-02-23 22:57:29 +0300

    pagination, sorting and filtering

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -21,18 +22,27 @@ namespace API.Controllers
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
         private readonly IPhotoService _photoService;
-
-        public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
+        public UsersController(IUserRepository userRepository, IMapper mapper, 
+            IPhotoService photoService)
         {
-            _userRepository = userRepository;
-            _mapper = mapper;
             _photoService = photoService;
+            _mapper = mapper;
+            _userRepository = userRepository;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _userRepository.GetMembersAsync();
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            userParams.CurrentUsername = user.Username;
+
+            if (string.IsNullOrEmpty(userParams.Gender))
+                userParams.Gender = user.Gender == "male" ? "female" : "male";
+
+            var users = await _userRepository.GetMembersAsync(userParams);
+
+            Response.AddPaginationHeader(users.CurrentPage, users.PageSize, 
+                users.TotalCount, users.TotalPages);
 
             return Ok(users);
         }
@@ -44,17 +54,18 @@ namespace API.Controllers
         }
 
         [HttpPut]
-        public async Task<ActionResult> UpdateUser(MemberUpdateDTO memberUpdateDTO) {
-            
+        public async Task<ActionResult> UpdateUser(MemberUpdateDTO memberUpdateDto)
+        {
+
             var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
-            _mapper.Map(memberUpdateDTO, user);
+            _mapper.Map(memberUpdateDto, user);
 
             _userRepository.Update(user);
 
-            if(await _userRepository.SaveAllAsync()) return NoContent();
+            if (await _userRepository.SaveAllAsync()) return NoContent();
 
-            return BadRequest("Failed to update user!");
+            return BadRequest("Failed to update user");
         }
 
         [HttpPost("add-photo")]
@@ -64,24 +75,28 @@ namespace API.Controllers
 
             var result = await _photoService.AddPhotoAsync(file);
 
-            if(result.Error != null) return BadRequest(result.Error.Message);
+            if (result.Error != null) return BadRequest(result.Error.Message);
 
-            var photo = new Photo{
+            var photo = new Photo
+            {
                 Url = result.SecureUrl.AbsoluteUri,
                 PublicId = result.PublicId
             };
 
-            if(user.Photos.Count == 0){
+            if (user.Photos.Count == 0)
+            {
                 photo.IsMain = true;
             }
 
             user.Photos.Add(photo);
 
-            if(await _userRepository.SaveAllAsync()){
-                return CreatedAtRoute("GetUser", new {username = user.Username}, _mapper.Map<PhotoDTO>(photo));
+            if (await _userRepository.SaveAllAsync())
+            {
+                return CreatedAtRoute("GetUser", new {username = user.Username} ,_mapper.Map<PhotoDTO>(photo));
             }
+                
 
-            return BadRequest("Problem has occured during add photo"!);
+            return BadRequest("Problem addding photo");
         }
 
         [HttpPut("set-main-photo/{photoId}")]
--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { NgxGalleryModule } from '@kolkov/ngx-gallery';
-import { FileUploadModule } from 'ng2-file-upload';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import {NgxGalleryModule} from '@kolkov/ngx-gallery';
+import {FileUploadModule} from 'ng2-file-upload';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { TimeagoModule } from 'ngx-timeago';
 
 @NgModule({
   declarations: [],
@@ -13,12 +16,15 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     CommonModule,
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
-        positionClass: 'toast-bottom-right'
-      }),
-      TabsModule.forRoot(),
-      NgxGalleryModule,
-      FileUploadModule,
-      BsDatepickerModule.forRoot()
+      positionClass: 'toast-bottom-right'
+    }),
+    TabsModule.forRoot(),
+    NgxGalleryModule,
+    FileUploadModule,
+    BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot(),
+    ButtonsModule.forRoot(),
+    TimeagoModule.forRoot()
   ],
   exports: [
     BsDropdownModule,
@@ -26,8 +32,10 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     TabsModule,
     NgxGalleryModule,
     FileUploadModule,
-    BsDatepickerModule
+    BsDatepickerModule,
+    PaginationModule,
+    ButtonsModule,
+    TimeagoModule
   ]
 })
-
 export class SharedModule { }
